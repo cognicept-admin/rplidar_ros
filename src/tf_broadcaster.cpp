b75f3d6cda0989464062d3fffa5b6a6a73038944
Yoeng Kok Leong <kokleong.yoeng@kabam.ai>
2024-12-20 10:37:26 +0800

    renamed tf node and param

--- src/tf_broadcaster.cpp
+++ src/tf_broadcaster.cpp
@@ -4,7 +4,7 @@
 #include <tf2/LinearMath/Quaternion.h>
 
 void broadcastStaticTransform(
-    double x, double y, double z, 
+    double x, double y, double z,
     double roll, double pitch, double yaw,
     const std::string& parent_frame, const std::string& child_frame)
 {
@@ -30,31 +30,31 @@ void broadcastStaticTransform(
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "side_lidar_tf");
+    ros::init(argc, argv, "tf_broadcaster");
     ros::NodeHandle nh("~");
 
     // Load parameters from the YAML file
     double x, y, z, roll, pitch, yaw;
-    std::string lidar_side, parent_frame, child_frame;
+    std::string lidar_name, parent_frame, child_frame;
 
-    if (!nh.getParam("lidar_side", lidar_side)) {
+    if (!nh.getParam("lidar_name", lidar_name)) {
         ROS_ERROR("Failed to get 'lidar_side' parameter.");
         return -1;
     }
 
-    ROS_INFO("Loaded lidar_side: %s", lidar_side.c_str());
+    ROS_INFO("Loaded lidar_side: %s", lidar_name.c_str());
 
     // Retrieve all transform parameters for the specified lidar_side
-    if (!nh.getParam(lidar_side + "/x", x) ||
-        !nh.getParam(lidar_side + "/y", y) ||
-        !nh.getParam(lidar_side + "/z", z) ||
-        !nh.getParam(lidar_side + "/roll", roll) ||
-        !nh.getParam(lidar_side + "/pitch", pitch) ||
-        !nh.getParam(lidar_side + "/yaw", yaw) ||
-        !nh.getParam(lidar_side + "/parent_frame", parent_frame) ||
-        !nh.getParam(lidar_side + "/child_frame", child_frame))
+    if (!nh.getParam(lidar_name + "/x", x) ||
+        !nh.getParam(lidar_name + "/y", y) ||
+        !nh.getParam(lidar_name + "/z", z) ||
+        !nh.getParam(lidar_name + "/roll", roll) ||
+        !nh.getParam(lidar_name + "/pitch", pitch) ||
+        !nh.getParam(lidar_name + "/yaw", yaw) ||
+        !nh.getParam(lidar_name + "/parent_frame", parent_frame) ||
+        !nh.getParam(lidar_name + "/child_frame", child_frame))
     {
-        ROS_ERROR("Failed to retrieve all parameters for '%s'.", lidar_side.c_str());
+        ROS_ERROR("Failed to retrieve all parameters for '%s'.", lidar_name.c_str());
         return -1;
     }
 
